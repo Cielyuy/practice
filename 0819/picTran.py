c16d1129ca9410276365670960d33fe03af92841
pangyao <482318285@qq.com>
2021-08-24 16:19:30 +0800

    0824

--- 0819/picTran.py
+++ 0819/picTran.py
@@ -33,7 +33,7 @@ for i in txtDirList:
                 # print(line)
                 # print(re_ZiMu.findall(line))
                 if re_ZiMu.findall(line) == []:
-                    print(line)
+                    # print(line)
                     l0.append((re_XiaoShu.findall(line))[0])
                     l1.append((re_XiaoShu.findall(line))[1])
                     # l2.append((re_XiaoShu.findall(line))[2])
@@ -64,11 +64,14 @@ df.to_excel(excelDir,index = False)
 print(len(df.columns))
 print(df.index)
 # print(df[df['flow']>'1000'])
+df[df.columns[0]] = df[df.columns[0]].apply(lambda x : (int(x)//100))# 把100步当作1步
 for i in df.columns:
-    print(i)
+    # print(i)
     if i == df.columns[0]:
         pass
     else:
+        # print(df[i])
+        df[i] = df[i].apply(lambda x:round(float(x),2))  #保留两位小数
         plt.figure(i)
         # ax = plt.subplot(111)
         # plt.title('pic test!')plt.figure("huihui")
@@ -85,10 +88,10 @@ for i in df.columns:
         # ax.yaxis.set_major_locator(ymajorFormatter)
         # a = plt.xlim()[0]
         # b = plt.xlim()[1]
-        plt.xticks(np.arange(100,16000,100))
+        # plt.xticks(np.arange(100,16000,1000))
         
         # c ,d = plt.ylim()[0],plt.ylim()[1]
-        plt.yticks(np.arange(0,100,5))
+        # plt.yticks(np.arange(0,100,5))
         # print(a,b,c,d)
 
 
@@ -104,4 +107,22 @@ for i in df.columns:
         print("done!")
         # a1 = 1
         # b1 = 2
-        c1 = 3
+
+##指定两组数值进行画图匹配
+print('指定若干组进行对比')
+plt.figure('2&3')
+plt.title('2&3')
+# p1 = plt.plot(df[df.columns[0]],df['cirPoint2'])
+# p2 = plt.plot(df[df.columns[0]],df['cirPoint3'])
+plt.plot(df[df.columns[0]],df['cirPoint2'])
+plt.plot(df[df.columns[0]],df['cirPoint3'])
+#xy轴名称
+plt.xlabel('step')
+plt.ylabel("value")
+#设置图例
+# plt.legend([p1,p2],['p2','p3'],loc='upper left')
+plt.legend(['p2','p3'],loc='upper right')
+plt.savefig('cirPoint2&3'+'.png')
+print("done23!")
+
+##含有point的所有数组进行对比
